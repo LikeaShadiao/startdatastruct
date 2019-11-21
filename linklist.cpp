252a18fa218d7a2436e72a3accc0d61167c0a7b9
LikeaShadiao <2401244500@qq.com>
2019-11-21 17:09:50 +0800

    nexttodelete

--- linklist.cpp
+++ linklist.cpp
@@ -94,7 +94,7 @@ LinkList* Getv(LinkList*head,ElemType x)
 	return p;
 }
 
-//插入单链表
+//给定位置插入单链表
 void Insert(LinkList*L,ElemType x,int i)
 {
 	LinkList*p,*r;
@@ -110,4 +110,93 @@ void Insert(LinkList*L,ElemType x,int i)
 	}
 }
 
-//
+//建立有序链表并将元素插入
+void Insertbs(LinkList*head,ElemType x)
+{
+	LinkList *p,*q,*s;
+	s=(LinkList*)malloc(sizeof(LinkList));
+	s->data=x;
+	s->next=NULL;
+	p=head->next;
+	q=head;
+	while(p!=NULL&&p->data<x)
+	{
+		q=p;
+		p=p->next;
+	}
+	if(p==NULL)
+		q->next=s;
+	else
+	{
+		q->next=s;
+		s->next=p;
+	}
+}
+
+//单链表的删除，给出要删除的第i个结点
+void Delete(LinkList*L,int i)
+{
+	LinkList*p=Get(L,i-1);
+	if(p!=NULL&&P->next!=NULL)
+	{
+		LinkList*r=p->next;
+		p->next=r->next;
+		free(r);
+	}
+	else
+		printf("the node of i donnot exit.");
+}
+
+/*循环链表 数据类型书上没有，想想没
+什么问题，数据类型那肯定是跟链表一样啊*/
+
+//直接上例题，在单循环链表的基础上把两个非空循环链表a,b合并成一个循环链表
+
+//第一种情况，函数参数是尾指针
+LinkList*connectt(LinkList*La,LinkList*Lb)
+{
+	La->next=Lb->next->next;
+	free(Lb->next);
+	Lb->next=La->next;
+	return Lb;
+}
+
+//第二种情况，函数参数是头指针
+LinkList*connecth(LinkList*La,LinkList*Lb)
+{
+	LinkList*p=La;
+	while(p->next!=La)
+		p=p->next;
+	p->next=Lb->next;
+	LinkList*q=Lb;//也可以直接用p，不再加这个q
+	while(q->next!=Lb)
+		q=q->next;
+	q->next=La;
+	free(Lb);
+	return La;
+}
+
+//双向链表
+//双向链表的数据结构类型
+typedef struct dnode
+{
+	ElemType data;
+	struct dnode *prior,*next;
+}dlinklist;
+dlinklist* head;
+
+
+//双向链表的后插
+void hinsert(dlinklist*p,ElemType x)
+{
+	dlinklist*s=(dlinklist*)malloc(sizeof(dlinklist));
+	s->data=x;
+	s->prior=p;
+	s->next=p->next;
+	p->next->prior=s;
+	p->next=s;
+}
+
+//前插差不多我就不写了，上面能看懂也就可以自己写了
+
+//双向链表的删除
